efcb09237e8cd81eb94c48e96ac4a31413bc5216
Asheq Imran <asheq.imran@ge.com>
2018-10-19 10:18:53 -0700

    Update dotfiles

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -1,7 +1,9 @@
-# TODO: Set fancy or non-fancy icons based on an $USE_FANCY_SYMBOLS.
-# TODO: Change color based on exit code of last command.
-# TODO: Show depth of nested shells.
-# TODO: Show time last command took to run.
+# TODO
+# - Set fancy or non-fancy icons based on an $USE_FANCY_SYMBOLS.
+# - Change color based on exit code of last command.
+# - Show depth of nested shells.
+# - Show time last command took to run.
+
 color_prompt=yes
 if [ "$color_prompt" = yes ]; then
 	PS1="\[\033[00;36m\]"
