59e740c4442d31d40b813dc26f0f42138dafb4bd
Asheq Imran <asheq.imran@ge.com>
2018-10-24 19:30:56 -0700

    Update dotfiles

--- .bash.d/prompt.sh
+++ .bash.d/prompt.sh
@@ -1,4 +1,4 @@
-# TODO
+# TODO:
 # - Set fancy or non-fancy icons based on an $USE_FANCY_SYMBOLS.
 # - Show depth of nested shells.
 # - Show time last command took to run.
@@ -7,41 +7,38 @@ PROMPT_COMMAND=__prompt_command
 
 __prompt_command() {
 	local EXIT="$?"
-	PS1=""
 
 	local reset_color='\[\e[0m\]'
-
-	if [ $EXIT != 0 ]; then
-		local main_color='\[\e[0;31m\]' # Red
-	else
-		local main_color='\[\e[0;36m\]' # Cyan
-	fi
-
 	local secondary_color='\[\e[0;02m\]' # Gray
-
-	PS1="${secondary_color}"
-	PS1+="\n"
-	PS1+="┌╴"
-	PS1+='`pwd_head`'
-	PS1+="${main_color}"
-	PS1+='`pwd_tail` '
-	PS1+="${secondary_color}"
-	PS1+="\`parse_git_branch\`"
-	PS1+='`[ \j -gt 0 ] && echo [ \ \j]\ `'
-	PS1+="[  \u] "
-	if [ -n "${SSH_CLIENT}" ] || [ -n "${SSH_TTY}" ]; then
-		PS1+="[  \h]"
+	if [ $EXIT != 0 ]; then
+		local primary_color='\[\e[0;31m\]' # Red
+	else
+		local primary_color='\[\e[0;36m\]' # Cyan
 	fi
-	PS1+="\n"
+
+	PS1='\n'
 	PS1+="${secondary_color}"
-	PS1+="└╴"
+	PS1+='┌╴'
+	PS1+='$(pwd_head)'
+	PS1+="${primary_color}"
+	PS1+='$(pwd_tail) '
+	PS1+="${secondary_color}"
+	PS1+='$(parse_git_branch)'
+	PS1+='$([ \j -gt 0 ] && echo [ \ \j]\ )'
+	PS1+='[  \u] '
+	if [ -n "${SSH_CLIENT}" ] || [ -n "${SSH_TTY}" ]; then
+		PS1+='[  \h]'
+	fi
+	PS1+='\n'
+	PS1+="${secondary_color}"
+	PS1+='└╴'
 	PS1+="${reset_color}"
 }
 
-# PS1='`pwd_head`'
-# PS1+="\[\e[0;36m\]"
-# PS1+='`pwd_tail`'
-# PS1+="\[\e[0m\]"
+# PS1='$(pwd_head)'
+# PS1+='\[\e[0;36m\]'
+# PS1+='$(pwd_tail)'
+# PS1+='\[\e[0m\]'
 # PS1+=' > '
 
 pwd_head() {
@@ -53,6 +50,7 @@ pwd_head() {
 		if [ "${head}" = '/' ]; then
 			head="/"
 		else
+			# TODO: Replace ${HOME} with ~
 			head="${head}/"
 		fi
 	fi
@@ -60,5 +58,5 @@ pwd_head() {
 }
 
 pwd_tail() {
-	echo "$(basename "${PWD}")"
+	basename "${PWD}"
 }
