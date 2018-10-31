27c221a0d6d01d6d228026a2b808aefb720bdb15
Asheq Imran <asheq.imran@ge.com>
2018-10-31 16:11:38 -0700

    Update dotfiles

--- .bashrc.d/first/internal/create-functions/__prompt_command.sh
+++ .bashrc.d/first/internal/create-functions/__prompt_command.sh
@@ -39,8 +39,7 @@ function pwd_head() {
 		l_head=''
 	else
 		l_head="$(dirname "${PWD}")"
-		# TODO: Only do the replacement from the start (just in case)
-		l_head="${l_head/${HOME}/\~}"
+		[[ "${l_head}" =~ ^"$HOME"(/|$) ]] && l_head="~${l_head#$HOME}"
 		if [ "${l_head}" = '/' ]; then
 			l_head="/"
 		else
