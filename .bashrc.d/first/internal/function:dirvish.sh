66871d5d88a1d3cea5a44dd5c665738d350ffe29
Asheq Imran <asheq.imran@ge.com>
2018-11-27 12:09:04 -0800

    Update dotfiles

---
+++ .bashrc.d/first/internal/function:dirvish.sh
@@ -0,0 +1,3 @@
+function dirvish() {
+	nvim -c 'Dirvish' -c 'autocmd VimLeave * call writefile([expand("%:h")], expand("$HOME") . "/.dirvishdir")'; LASTDIR=`cat ${HOME}/.dirvishdir`; cd "${LASTDIR}"
+}
