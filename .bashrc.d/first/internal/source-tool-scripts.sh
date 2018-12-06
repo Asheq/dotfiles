9a9f8a99def7cd37da7b50785cef014d4549dd8b
Asheq Imran <asheq.imran@ge.com>
2018-12-06 13:26:04 -0800

    Update dotfiles

--- .bashrc.d/first/internal/source-tool-scripts.sh
+++ .bashrc.d/first/internal/source-tool-scripts.sh
@@ -6,5 +6,5 @@
 # ------------------------------------------------------------------------------
 # nvm
 # ------------------------------------------------------------------------------
-[[ -s "$NVM_DIR/nvm.sh" ]]          && source "$NVM_DIR/nvm.sh"
+[[ -s "$NVM_DIR/nvm.sh" ]]          && source "$NVM_DIR/nvm.sh" --no-use
 [[ -s "$NVM_DIR/bash_completion" ]] && source "$NVM_DIR/bash_completion"
