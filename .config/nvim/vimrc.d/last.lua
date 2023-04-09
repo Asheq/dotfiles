5c60d147fd6951707a2d10d2f79fd526b0320089
Asheq Imran <asheq89@gmail.com>
2023-04-08 20:35:38 -0700

    Update

--- .config/nvim/vimrc.d/last.lua
+++ .config/nvim/vimrc.d/last.lua
@@ -1,13 +1,13 @@
-require("github-theme").setup({
-  theme_style = "dark",
-  hide_inactive_statusline = false,
-  dark_float = true,
-  overrides = function(c)
-    return {
-      DiffText = { fg = c.diff.change, bg = c.diff.change_fg },
-    }
-  end
-})
+-- require("github-theme").setup({
+--   theme_style = "light",
+--   hide_inactive_statusline = false,
+--   dark_float = true,
+--   overrides = function(c)
+--     return {
+--       DiffText = { fg = c.diff.change, bg = c.diff.change_fg },
+--     }
+--   end
+-- })
 
 require("colorizer").setup()
 
--- .config/nvim/vimrc.d/last.vim
+++ .config/nvim/vimrc.d/last.vim
@@ -1,7 +1,10 @@
 autocmd TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", timeout=300, on_visual=true}
 
-luafile <sfile>:h/last.local.lua
+set background=light
+colorscheme gruvbox
 
-" It seems I have to do this here, and not in last.local.lua. Otherwise, the
+luafile <sfile>:h/last.lua
+
+" It seems I have to do this here, and not in last.lua. Otherwise, the
 " color gets overriden
-highlight link htmlH2 Function
+" highlight link htmlH2 Function
