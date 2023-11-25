b41d3fea22f5f546ac066d264abb003cfc38e9d6
Asheq Imran <asheq89@gmail.com>
2023-11-24 22:32:30 -0800

    Update

--- .config/nvim/vimrc.d/final.lua
+++ .config/nvim/vimrc.d/final.lua
@@ -5,3 +5,16 @@
 -- else
 --     -- print('Error: Module "colorizer" not found')
 -- end
+
+-- TODO-L: Uncomment these and put in groups in order to save and restore manual folds
+-- vim.api.nvim_create_autocmd({ "BufUnload" }, {
+--     callback = function()
+--         vim.cmd([[silent! mkview 1]])
+--     end,
+-- })
+-- vim.api.nvim_create_autocmd({ "BufRead" }, {
+--     callback = function()
+--         vim.cmd([[silent! loadview 1]])
+--     end,
+-- })
+
