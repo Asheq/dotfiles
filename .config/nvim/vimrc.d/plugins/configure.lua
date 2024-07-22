31b9c3e4c7ecfa0906b10f58e4a7f4e1c8ae0bf4
Asheq Imran <asheq89@gmail.com>
2024-07-22 12:03:26 -0700

    Update

--- .config/nvim/vimrc.d/plugins/configure.lua
+++ .config/nvim/vimrc.d/plugins/configure.lua
@@ -1,3 +1,18 @@
 -- TODO-L: Guard
-require("oil").setup()
+require("oil").setup({
+    default_file_explorer = true,
+    view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name)
+            if name == ".DS_Store" then
+                return true
+            end
+            if name == ".." then
+                return true
+            end
+            return false
+        end,
+    }
+})
+
 vim.keymap.set("n", "-", "<CMD>Oil<Return>", { desc = "Open parent directory" })
