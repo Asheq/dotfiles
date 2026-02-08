6976eea967c33b65538912721508c49a3d5d8a8b
Asheq Imran <asheq89@gmail.com>
2026-02-07 17:27:44 -0800

    Update

--- .config/nvim/init/commands.lua
+++ .config/nvim/init/commands.lua
@@ -4,4 +4,3 @@ vim.api.nvim_create_user_command("PrintGeneralOptions", options.print_general, {
 vim.api.nvim_create_user_command("PrintFormattingOptions", options.print_formatting, {})
 vim.api.nvim_create_user_command("PrintIndentingOptions", options.print_indenting, {})
 vim.api.nvim_create_user_command("PrintFoldingOptions", options.print_folding, {})
-vim.api.nvim_create_user_command("PrintUpdatedOptions", options.print_updated, {})
