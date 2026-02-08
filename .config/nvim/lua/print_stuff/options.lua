ab8c2aef79f458b4d620ff165c9405ae7d59a827
Asheq Imran <asheq89@gmail.com>
2026-02-07 22:12:04 -0800

    Update

--- .config/nvim/lua/print_stuff/options.lua
+++ .config/nvim/lua/print_stuff/options.lua
@@ -1,18 +1,139 @@
 local M = {}
 
-local function echo_line(chunks)
-	vim.api.nvim_echo(chunks, true, {})
+local function echo_line(chunks, indent)
+	local indent_str = string.rep("  ", indent and (indent * 2) or 0)
+	local indented_chunks = vim.deepcopy(chunks)
+	table.insert(indented_chunks, 1, { indent_str, "Normal" })
+	vim.api.nvim_echo(indented_chunks, true, {})
+end
+
+local function get_filename(sid)
+	if sid == 0 then
+		return nil
+	end
+
+	if sid < 0 then
+		return sid
+	end
+
+	local scripts = vim.fn.getscriptinfo({ sid = sid })
+
+	if scripts[1] and scripts[1].name and scripts[1].name ~= "" then
+		local name = scripts[1].name
+
+		local runtime = vim.env.VIMRUNTIME
+		if runtime and runtime ~= "" and name:sub(1, #runtime) == runtime then
+			name = "$VIMRUNTIME" .. name:sub(#runtime + 1)
+		end
+
+		local vimconfig = vim.env.HOME .. "/.config/nvim"
+		if name:sub(1, #vimconfig) == vimconfig then
+			name = "$VIMCONFIG" .. name:sub(#vimconfig + 1)
+		end
+
+		local plug_home = vim.g.plug_home
+		if name:sub(1, #plug_home) == plug_home then
+			name = "$PLUGHOME" .. name:sub(#plug_home + 1)
+		end
+
+		return name
+	end
+
+	return sid
+end
+
+local show_default_value = true
+local show_used_value = true
+local show_initial_value = true
+
+local function print_option(optname)
+	local info = vim.api.nvim_get_option_info2(optname, {})
+	local value = vim.api.nvim_get_option_value(optname, {})
+
+	local global_info = vim.api.nvim_get_option_info2(optname, { scope = "global" })
+	local global_value = vim.api.nvim_get_option_value(optname, { scope = "global" })
+
+	local local_info = vim.api.nvim_get_option_info2(optname, { scope = "local" })
+	local local_value = vim.api.nvim_get_option_value(optname, { scope = "local" })
+
+	local was_set_by_script =
+		info.last_set_sid ~= 0 or
+		global_info.last_set_sid ~= 0 or
+		local_info.last_set_sid ~= 0
+
+	echo_line({
+		{ " " .. optname .. " ",         "TermCursor" },
+		{ " (" .. info.shortname .. ")", "SpecialKey" },
+		{ " " .. info.type,              "SpecialKey" }
+	}, 1)
+
+	if (show_used_value) then
+		echo_line(
+			{
+				{ "   Used: ",     "Normal" },
+				{ tostring(value), "String" },
+			},
+			2)
+	end
+
+	if info.scope == "buf" or info.scope == "win" then
+		local local_scope_label = info.scope == "buf" and " Buffer" or " Window"
+
+		local local_last_set_sid = local_info.last_set_sid
+		local local_was_set_by_script = local_info.last_set_sid ~= 0
+		local local_last_set_filename = get_filename(local_last_set_sid)
+
+		echo_line(
+			{
+				{ local_scope_label .. ": ", "Normal" },
+				{ tostring(local_value),     "SpecialKey" },
+				local_last_set_filename and { " ➤ ", "DiagnosticError" },
+				local_last_set_filename and { tostring(local_last_set_filename), "DiagnosticError" },
+			},
+			2)
+	end
+
+	if (info.scope == "global" or info.global_local or show_initial_value) then
+		local global_scope_label = ""
+		if (info.scope == "global" or info.global_local) then
+			global_scope_label = " Global"
+		else
+			global_scope_label = "Initial"
+		end
+
+		local global_last_set_sid = global_info.last_set_sid
+		local global_was_set_by_script = global_info.last_set_sid ~= 0
+		local global_last_set_filename = get_filename(global_last_set_sid)
+
+		echo_line(
+			{
+				{ global_scope_label .. ": ", "Normal" },
+				{ tostring(global_value),     "SpecialKey" },
+				global_last_set_filename and { " ➤ ", "DiagnosticError" },
+				global_last_set_filename and { tostring(global_last_set_filename), "DiagnosticError" },
+			},
+			2)
+	end
+
+	if (show_default_value) then
+		echo_line(
+			{
+				{ "Default: ",            "Normal" },
+				{ tostring(info.default), "SpecialKey" },
+			},
+			2)
+	end
 end
 
 local function print_option_groups(groups)
 	for _, group in ipairs(groups) do
 		if group.title and group.title ~= "" then
-			echo_line({ { group.title, "Underlined" } })
+			local extra_space = math.ceil((vim.o.columns - #group.title) / 2)
+			echo_line({ { group.title .. string.rep(" ", extra_space), "Underlined" } }, 0)
 		end
 
 		for _, optname in ipairs(group.options) do
-			local info = vim.api.nvim_get_option_info2(optname, {})
-			-- TODO: Print information abou the option
+			print_option(optname)
 		end
 	end
 end
