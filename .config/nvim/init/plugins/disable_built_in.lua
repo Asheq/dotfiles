d8d8d9eb12ea63f19d7c28a3c5135514589b1712
Asheq Imran <asheq89@gmail.com>
2026-02-27 09:04:36 -0800

    Update

--- .config/nvim/init/plugins/disable_built_in.lua
+++ .config/nvim/init/plugins/disable_built_in.lua
@@ -2,10 +2,10 @@
 -- g:loaded_xxx = 1 before that happens prevents a plugin from loading.
 -- ============================================================================
 
--- Do not load netrw
-vim.g.loaded_netrwPlugin = 1
+-- I could disable netrw since I do not use its primary functionality, but it
+-- has secondary features that are needed to download spell files.
+-- vim.g.loaded_netrwPlugin = 1
 
--- Do not load matchit and matchparen. They are both replaced with
--- andymass/vim-matchup.
+-- I will replace matchit and matchparen with andymass/vim-matchup.
 -- vim.g.loaded_matchit = 1
 -- vim.g.loaded_matchparen = 1
