ac0fe289fd4fb8c8981b829e52b2f3471b78c2f0
Asheq Imran <asheq89@gmail.com>
2026-03-02 14:29:13 -0800

    Update

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,4 +1,1 @@
-local config_path = vim.fn.stdpath("config")
-
 vim.opt_local.spell = true
-vim.opt_local.spellfile:append(config_path .. "/spell/programming.utf-8.add")
--- .config/nvim/after/ftplugin/vim.lua
+++ .config/nvim/after/ftplugin/vim.lua
@@ -1,5 +1,2 @@
-local config_path = vim.fn.stdpath("config")
-
 vim.opt_local.spell = true
 vim.opt_local.spelllang:append("vim")
-vim.opt_local.spellfile:append(config_path .. "/spell/programming.utf-8.add")
