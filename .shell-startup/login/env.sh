9410d026b29f2cf4939f4f5f97eea125de21e18e
Asheq Imran <Asheq89@gmail.com>
2026-03-08 20:00:36 -0700

    Update

--- .shell-startup/login/env.sh
+++ .shell-startup/login/env.sh
@@ -3,8 +3,8 @@
 export EDITOR='nvim-smart-open'
 export VISUAL="$EDITOR"
 
-# TODO: Replace with 'nvim-smart-open +Man!' when latest Neovim release allows
-# +{cmd} to be used with --remote:
+# Using less for now. TODO: Switch to 'nvim-smart-open +Man!' once the latest
+# Neovim release allows +{cmd} to be used with --remote.
 export MANPAGER='less'
 
 export NVM_DIR=~/.nvm
