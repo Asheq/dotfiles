7c6b63af9b87cd44190040e9bcc8d7fd9305f143
Asheq Imran <asheq89@gmail.com>
2026-04-08 15:08:34 -0700

    Update

--- .config/nvim/init/options/set_basic.lua
+++ .config/nvim/init/options/set_basic.lua
@@ -1,44 +1,51 @@
 -- Global
 -- ----------------------------------------------------------------------------
+-- Boolean
 vim.opt.exrc = true
 vim.opt.ruler = false
-vim.opt.timeout = false
-vim.opt.wrapscan = false
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.report = 1
-vim.opt.linespace = 5
-vim.opt.wildcharm = 26 -- 26 = ASCII decimal value of <C-z> key
+vim.opt.timeout = false
+vim.opt.wrapscan = false
+-- Number
 vim.opt.helpheight = 0
-vim.opt.jumpoptions = "stack"
+vim.opt.report = 1
+vim.opt.wildcharm = 26 -- 26 = ASCII decimal value of <C-z> key
+-- String
 vim.opt.guifont = "PragmataPro Mono:h22"
-vim.opt.wildignore = ".DS_Store,.tmp.drivedownload,.tmp.driveupload"
+vim.opt.jumpoptions = "stack"
 vim.opt.tabline = "%{%v:lua.require('lines').get_tabline()%}"
+vim.opt.wildignore = ".DS_Store,.tmp.drivedownload,.tmp.driveupload"
 
 -- Tab [+ Global]
 -- ----------------------------------------------------------------------------
+-- Number
 vim.opt.cmdheight = 2
 
 -- Window [+ Global]
 -- ----------------------------------------------------------------------------
+-- Boolean
+vim.opt.cursorline = true
 vim.opt.list = true
 vim.opt.number = true
-vim.opt.cursorline = true
 vim.opt.smoothscroll = true
+-- Number
 vim.opt.conceallevel = 2
-vim.opt.colorcolumn:append("+1")
+-- String
+vim.opt.foldcolumn = "auto:5"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldmethod = "expr"
 vim.opt.foldtext = ""
 vim.opt.signcolumn = "auto:5"
-vim.opt.foldcolumn = "auto:5"
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.statusline = "%{%v:lua.require('lines').get_statusline()%}"
 
 -- Buffer [+ Global]
 -- ----------------------------------------------------------------------------
+-- Boolean
 vim.opt.undofile = true
-vim.opt.grepprg = "rg --vimgrep"
-vim.opt.spelloptions = "camel,noplainbuffer"
-vim.opt.spelllang = "en_us"
-vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+-- String
 vim.opt.dictionary:append("/usr/share/dict/words")
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+vim.opt.spelllang = "en_us"
+vim.opt.spelloptions = "camel,noplainbuffer"
